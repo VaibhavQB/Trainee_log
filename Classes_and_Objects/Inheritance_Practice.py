8676a4f0e0c871fb28f22f9ceda0e5bcf55838f6
VaibhavQB <vaibhav.quantumbot@gmail.com>
2025-06-09 18:50:25 +0530

    Basics of Inheritance

---
+++ Classes_and_Objects/Inheritance_Practice.py
@@ -0,0 +1,63 @@
+# Simple Code to demonstrate polymorphism
+
+'''class Person():
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return  f"Name : {self.name}\nAge = {self.age}"
+
+class Employee(Person) :      # We need to give the parent class as the parameter to the child class
+    pass
+
+E1 = Employee("Casey", "27")
+print(E1)'''
+
+
+# Over-ridding the __init__ method and adding method 'details()' to the child class
+
+'''class Person():
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Employee(Person) :      # We need to give the parent class as the parameter to the child class
+    def __init__(self, name, pos):
+        self.name = name
+        self.pos = pos
+
+    def details(self):
+        return  f"Name : {self.name}\nPosition : {self.pos}"
+
+        
+E1 = Employee("Casey", "AI/ML Intern")
+print(E1.details())'''
+
+
+# Using super() function
+
+'''class Person():
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+class Employee(Person) :      # We need to give the parent class as the parameter to the child class
+    def __init__(self, name, age, pos):
+        super().__init__(name, age)
+        self.name = name
+        self.age = age
+        self.pos = pos
+
+    def details(self):
+        return  f"Name : {self.name}\nAge : {self.age}\nPosition : {self.pos}"
+
+        
+E1 = Employee("Casey",27, "AI/ML Intern")
+print(E1.details())'''
+
+
+# 
