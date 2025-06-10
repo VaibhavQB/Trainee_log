9598347b4687b9b9a77babc4b7534a7b2eb1767d
VaibhavQB <vaibhav.quantumbot@gmail.com>
2025-06-10 10:50:31 +0530

    Ploymorphism Practice Code

--- Classes_and_Objects/Polymorphism_Practice.py
+++ Classes_and_Objects/Polymorphism_Practice.py
@@ -1,6 +1,6 @@
 # A Simple code to show Pollymorphism
 
-class Car:
+'''class Car:
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
@@ -20,6 +20,39 @@ class Plane:
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
+    
+    def move(self) :
+        print("fly!!!")
+
+c = Car("Dodge", "Challanger")
+b = Boat("Sea Ray", "Sundancer 320")
+p  = Plane("Airbus","A380")
+
+for x in (c,b,p):
+    x.move()'''
+
+
+# Polymorphism in All Child class from sma e Parent Class
+
+class Vehicle:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Move!")
+
+class Car(Vehicle):
+    
+    def move(self) :
+        print("Drive!!!")
+
+class Boat(Vehicle):
+    
+    def move(self) :
+        print("Sail!!!")
+
+class Plane(Vehicle):
     
     def move(self) :
         print("fly!!!")
